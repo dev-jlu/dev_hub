b3742521504d8bc83acad3e8ec871d1479a21803
Jose Luis Perez <jperez.arkus@gmail.com>
2025-11-20 16:28:39 -0800

    Changes on cors.rb for allowed urls

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("CLIENT_URL") # React dev server
+    origins((ENV.fetch("CLIENT_URL", "").split(",")))
 
     resource "*",
       headers: :any,
