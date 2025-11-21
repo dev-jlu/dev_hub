337a4bd3dd4fe132488135b62b8c82c4a659c84e
Jose Luis Perez <jperez.arkus@gmail.com>
2025-11-20 17:17:46 -0800

    Removing code from user_session

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,7 +1,2 @@
 class UserSession < Authlogic::Session::Base
-  self.cookie_secure = Rails.env.production?
-  self.cookie_samesite_protection = :none
-  if Rails.env.production?
-    self.cookie_domain = ".onrender.com"
-  end
 end
--- config/initializers/authlogic.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.to_prepare do
-  Authlogic::Session::Base.controller = Authlogic::ControllerAdapters::RailsAdapter.new(self)
-end
